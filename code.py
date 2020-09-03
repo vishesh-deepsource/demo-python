7e6c41c20dac34222b7479e03128c6c230463bc6
Jai Pradeesh <68369919+jai-deepsource@users.noreply.github.com>
2020-09-03 14:02:13 +0530

    Update code.py

--- code.py
+++ code.py
@@ -11,8 +11,7 @@ AWS_SECRET_KEY = "d6s$f9g!j8mg7hw?n&2"
 
 class BaseNumberGenerator:
     def __init__(self):
-        self.limits = (1, 10)
-
+        self.limits = (1,10)
     def get_number(self, min_max):
         raise NotImplemented
 
@@ -21,7 +20,7 @@ class RandomNumberGenerator:
     def limits(self):
         return self.limits
 
-    def get_number(self, min_max=[1, 10]):
+    def get_number(self, min_max=[1,10]):
         """Get a random number between min and max."""
         assert all([isinstance(i, int) for i in min_max])
         return random.randint(*min_max)
