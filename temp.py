793f35ca4b0d1b21777408e3fadfee176c030582
Sanket Saurav <sanketsaurav@gmail.com>
2022-02-22 15:37:37 +0530

    fix issues

--- temp.py
+++ temp.py
@@ -1,6 +1,9 @@
 import os
 
 
-def Foo(num_1, num_2, baz=[1, 2]):
+def Foo(num_1, num_2, baz=None):
+    """This is a sample function."""
+    if baz is None:
+        baz = [1, 2]
     print(os.getenv(num_1))
     return baz == [num_1, num_2]
