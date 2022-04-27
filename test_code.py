4ac19ce366c66bb46b2b515d71d19671405363b1
Srijan Saurav <srijan@deepsource.io>
2022-04-27 17:02:27 +0530

    Change coverage report format to lcov

---
+++ test_code.py
@@ -0,0 +1,6 @@
+from spaghetti_code import RandomNumberGenerator
+
+
+def test_random_number_generator():
+    """Test random number generator."""
+    assert RandomNumberGenerator().get_number() != RandomNumberGenerator().get_number()
